2e1b247de7bdb746105920f4667450634b80273a
JohnfyBUPT <zhfy888@163.com>
2016-04-11 10:22:47 +0800

    now has "naive" intelligence

---
+++ app/src/main/java/com/bupt/johnfrey/whisper/NaiveBayesian/TrainData.java
@@ -0,0 +1,44 @@
+package com.bupt.johnfrey.whisper.NaiveBayesian;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class TrainData {
+	private List<List<String>> trainData;
+	private List<Integer> trainDataClass;
+
+	public void init(){
+		trainData = new ArrayList<List<String>>();
+		trainDataClass = new ArrayList<Integer>();
+		trainData.add(Arrays.asList(new String[]{"my","dog","has","flea"}));
+		trainData.add(Arrays.asList(new String[]{"you","are","an","ass","ass"}));
+		trainData.add(Arrays.asList(new String[]{"don't","touch","him","asshole"}));
+		trainData.add(Arrays.asList(new String[]{"take","him","for","a","walk"}));
+		trainDataClass.add(0);
+		trainDataClass.add(1);
+		trainDataClass.add(1);
+		trainDataClass.add(0);
+	}
+	
+	public void print(){
+		for(int i = 0;i<trainDataClass.size();i++){
+			System.out.println(trainData.get(i)+trainDataClass.get(i).toString());
+		}
+	}
+	
+	public void addEntry(int dataClass,String... data){
+		trainDataClass.add(dataClass);
+		List<String> mdata = new ArrayList<String>();
+		for(String word:data){
+			mdata.add(word);
+		}
+		trainData.add(mdata);
+	}
+	
+	public List<List<String>> getData(){
+		return trainData;
+	}
+	
+	public List<Integer> getDataClass(){
+		return trainDataClass;
+	}
+}
---
+++ app/src/main/java/com/bupt/johnfrey/whisper/NaiveBayesian/Vocabulary.java
@@ -0,0 +1,26 @@
+package com.bupt.johnfrey.whisper.NaiveBayesian;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vocabulary {
+	private List<String> vocabulary;
+	
+	public void init(List<List<String>> datas){
+		vocabulary = new ArrayList<String>();
+		for(List<String> data:datas){
+			for(String word:data){
+				if(!vocabulary.contains(word)){//ʵ�ֲ���
+					vocabulary.add(word);
+				}
+			}
+		}
+	}
+	
+	public void print(){
+		System.out.print(vocabulary);
+	}
+	
+	public List<String> get(){
+		return vocabulary;
+	}
+}
